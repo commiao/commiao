7f015d3241f81970ef6ee262bde2e325f4003707
jingmiao <jingmiao@chtwm.com>
2017-09-20 09:38:58 +0800

    container.parent

---
+++ commiao.parent/controller.parent/controller.truck/src/main/java/com/commiao/controller/truck/App.java
@@ -0,0 +1,13 @@
+package com.commiao.controller.truck;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
