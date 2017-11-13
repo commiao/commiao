5e2f27db990be2b94b7a03b4697b75aea3ba6069
jingmiao <jingmiao@chtwm.com>
2017-11-13 15:37:35 +0800

    redisMQ模块

--- basic.parent/basic.arch.parent/basic.arch.component/basic.arch.component.cache/src/main/java/basic/arch/component/cache/redis/impl/RedisMqService.java
+++ basic.parent/basic.arch.parent/basic.arch.component/basic.arch.component.cache/src/main/java/basic/arch/component/cache/redis/impl/RedisMqService.java
@@ -2,7 +2,35 @@ package basic.arch.component.cache.redis.impl;
 
 public interface RedisMqService {
 	
-	<T>void save(T bean);
-    <T>T read(T bean);
+	/** 
+	 * @Description::(从队列左侧插入一个对象). <br/> 
+	 * @author jingmiao
+	 * @param key
+	 * @param bean
+	 */ 
+	<V>void pushFromLeft(String key, V bean);
+    /** 
+     * @Description::(从队列右侧移除一个对象). <br/> 
+     * @author jingmiao
+     * @param key
+     * @param clazz
+     * @return
+     */ 
+    <V>V removeFromRight(String key, Class<V> clazz);
+    /** 
+     * @Description::(从队列右侧插入一个对象). <br/> 
+     * @author jingmiao
+     * @param key
+     * @param bean
+     */ 
+    <V>void pushFromRight(String key, V bean);
+	/** 
+	 * @Description::(从队列左侧移除一个对象). <br/> 
+	 * @author jingmiao
+	 * @param key
+	 * @param clazz
+	 * @return
+	 */ 
+	<V>V removeFromLeft(String key, Class<V> clazz);
     
 }
